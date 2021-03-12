2d870f5fb5a8b51a0411efa9c224c08fdcf07443
Marcin Stanik <mstanik25@gmail.com>
2021-03-12 23:17:05 +0100

    fixed overflow

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 const header = document.querySelector(".header")
 window.addEventListener('scroll', () => {
     if (window.pageYOffset > 10) {
-        header.style = "padding:1rem 2rem;font-size:0.9em";
+        header.style = "padding:1rem 3rem;font-size:0.9em";
     } else {
-        header.style = "padding:1.5rem";
+        header.style = "padding:1.5rem 3rem";
     }
 })
 const observerOptions = {
